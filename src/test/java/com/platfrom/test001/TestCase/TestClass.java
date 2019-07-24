46507e312a729bda702f59b74d18a0ee7c4b3474
luoxinge <657988208@qq.com>
2019-07-24 19:27:04 +0800

    666

--- src/test/java/com/platfrom/test001/TestCase/TestClass.java
+++ src/test/java/com/platfrom/test001/TestCase/TestClass.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
