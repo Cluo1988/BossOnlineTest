4ea1449ba5c1f4e4d48cb4d69baf77f57e808716
luoxinge <657988208@qq.com>
2019-07-31 10:22:34 +0800

    666

--- src/test/java/com/platfrom/test001/FindBy/Cont.java
+++ src/test/java/com/platfrom/test001/FindBy/Cont.java
@@ -191,7 +191,6 @@ public class Cont {
         ClassAll.sleep(10000);
         contsave.click();
         ClassAll.sleep(10000);
-        ClassAll.sleep(10000);
         receipt.click();
         ClassAll.sleep(10000);
         receiptampus.sendKeys(Receiptampus);
